9234b383e053857d76bf3e070edc17adab282a45
alexander-maximov <alexander.maximov@devtodev.com>
2021-03-19 21:42:31 +0300

    Init

---
+++ TestplanPackageCounter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestplanPackageCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
